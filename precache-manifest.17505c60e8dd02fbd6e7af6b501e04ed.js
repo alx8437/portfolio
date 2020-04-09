7f9511681508ec7911787ad88aa32bfca3274590
Aleksandr <alx8437@mail.ru>
2020-04-09 11:40:44 +0300

    Updates

---
+++ precache-manifest.17505c60e8dd02fbd6e7af6b501e04ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d8d04a61125027b62fd9bc5f42da8f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba898747c284c01c65b2",
+    "url": "/portfolio/static/css/main.f54e7198.chunk.css"
+  },
+  {
+    "revision": "3d28094313bee65ab2be",
+    "url": "/portfolio/static/js/2.57ccaa29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.57ccaa29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba898747c284c01c65b2",
+    "url": "/portfolio/static/js/main.ab3a1c5f.chunk.js"
+  },
+  {
+    "revision": "68b62cd748292e25828d",
+    "url": "/portfolio/static/js/runtime-main.94a0eae4.js"
+  }
+]);
