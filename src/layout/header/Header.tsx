dc2143116e35473539f4b451055ad9a24185db7a
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-10 15:40:49 +0300

    02 Container was added

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import {Logo} from "../../components/Logo/Logo.tsx";
 import {Menu} from "../../components/Menu/Menu.tsx";
+import {Container} from "../../components/Container.tsx";
+import {FlexWrapper} from "../../components/StyledComponents/FlexWrapper.tsx";
 
 export const Header = () => {
     const menuItems = ['Home', 'Skills', 'Works', 'Testimony', 'Contact']
 
     return (
         <StyledHeader>
-            <Logo />
-            <Menu menuItems={menuItems} />
+            <Container >
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <Menu menuItems={menuItems}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
