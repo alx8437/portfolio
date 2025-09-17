f951e5fae6da4b2be0feddc425a640b10e849aa6
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-17 15:22:50 +0300

    02 Main was completed

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,19 +1,24 @@
 import styled from "styled-components";
 import photo from '../../../assets/images/garrett-parker-DlkF4-dbCOU-unsplash.webp'
 import {FlexWrapper} from "../../../components/StyledComponents/FlexWrapper.tsx";
+import {Container} from "../../../components/Container.tsx";
+import {theme} from "../../../styles/Theme.ts";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>Lorem ipsum dolor</Name>
-                    <MainTitle>Sed ut perspiciatis</MainTitle>
-                </div>
-
-                <Photo src={photo} alt="photo"/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"}>
+                    <TextBlock>
+                        <SmallText>Hi There</SmallText>
+                        <Name>Lorem <span>ipsum dolor</span></Name>
+                        <MainTitle>Sed ut perspiciatis</MainTitle>
+                    </TextBlock>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt="photo"/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -21,6 +26,7 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #e6dbdb;
+    display: flex;
 `
 
 const Photo = styled.img`
@@ -29,11 +35,60 @@ const Photo = styled.img`
     object-fit: cover;
 `
 
-const MainTitle = styled.h1`
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
     
+    &::before {
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        content: "";
+        border: 5px solid ${theme.color.accent};
+        width: 360px;
+        height: 470px;
+        z-index: -1;
+    }
+`
+
+const MainTitle = styled.h1`
+    font-weight: 400;
+    font-size: 27px;
 `
 
 const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
     
+    span {
+        position: relative;
+        z-index: 0;
+        
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.color.accent};
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+`
+
+const SmallText = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+    text-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+`
+
+const TextBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 `
 
