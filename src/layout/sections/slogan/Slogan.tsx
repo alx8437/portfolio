b10648d07f3330dd97c06806c9a74d10bf05c361
Aleks Zelenskiy <dezion777@gmail.com>
2025-11-26 15:15:17 +0300

    02 Slogan block was completed

--- src/layout/sections/slogan/Slogan.tsx
+++ src/layout/sections/slogan/Slogan.tsx
@@ -1,17 +1,22 @@
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/StyledComponents/SectionTitle.tsx";
 import {Button} from "../../../components/StyledComponents/Button.tsx";
+import {Container} from "../../../components/Container.tsx";
+import {FlexWrapper} from "../../../components/StyledComponents/FlexWrapper.tsx";
 
 export const Slogan = () => {
     return (
-        <StyledSlogan>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
-            <Button>Hire me</Button>
-        </StyledSlogan>
+            <StyledSlogan>
+                <Container>
+                    <FlexWrapper direction={'column'} align={'center'}>
+                        <SectionTitle>I Am Available For Freelance</SectionTitle>
+                        <Button>Hire me</Button>
+                    </FlexWrapper>
+                </Container>
+            </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
     min-height: 30vh;
-    background-color: #8a7e80;
 `
